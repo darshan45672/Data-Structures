8a95ec11684fbe3d22e3550cd51d1f6f28ec5873
darshan45672 <drshnbhandary@gmail.com>
2023-05-14 09:23:41 +0530

    updated file linear search

---
+++ linearSearch.c
@@ -0,0 +1,40 @@
+/* Program to perform linear search */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int linear( int key, int a[], int n){
+    for (int i = 0; i < n; i++){
+        /* code */
+        if (key == a[i])    {
+            /* code */
+            return i; 
+        }
+
+        return -1;
+    }
+}
+
+int main(void){
+    int n, key, a[20], i, pos;
+    printf("\nEnter the value of n:\t");
+    scanf("%d",&n);
+    printf("Enter the values \n");
+    for (int i = 0; i < n; i++){
+        /* code */
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the item to be searched\n");
+    scanf("%d",&key);
+
+    pos=linear(key, a, n);
+    if (pos==-1){
+        /* code */
+        printf("\nItem not found!!\n");
+    }else{
+        printf("Item found!!\n");
+    }
+    
+ return 0;
+}
